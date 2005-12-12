e77c07719a4168d51bcd5f7481b22331ac5e0506
sukria <sukria@2e458433-d701-0410-9826-f9b593394a3c>
2005-12-12 09:40:55 +0000

    adding the tests for external hooks
    git-svn-id: svn://svn.backup-manager.org/backup-manager/trunk@274 2e458433-d701-0410-9826-f9b593394a3c

---
+++ t/t05-hooks.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+
+# Each test script should include testlib.sh
+source testlib.sh
+# When the test is ready, set this to false for nice outputs.
+# if you want to see what happens, use those flags
+ verbose="true"
+ warnings="true"
+
+# The conffile part of the test, see confs/* for details.
+source confs/base.conf
+export BM_PRE_BACKUP_COMMAND="/bin/true"
+export BM_POST_BACKUP_COMMAND="echo true"
+
+# The test actions
+bm_init_env
+exec_pre_command
+exec_post_command
+
+exit 0

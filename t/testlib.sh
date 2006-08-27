e2c9dc015b05afad313a6dce16b8c6b58a77f7c2
sukria <sukria@2e458433-d701-0410-9826-f9b593394a3c>
2006-08-27 10:53:53 +0000

    2006-08-27  Alexis Sukrieh  <sukria@backup-manager.org>
    	* backup-manager:
    	  + Replaced static paths to external programs by $(which statement).
    	    (closes: #114)
    	* t/testlib.sh:
    	  + Applied the same changes to the teslib.sh library so the test scripts
    	    behave the same way.
    
    
    
    
    git-svn-id: svn://svn.backup-manager.org/backup-manager/trunk@471 2e458433-d701-0410-9826-f9b593394a3c

--- t/testlib.sh
+++ t/testlib.sh
@@ -13,34 +13,38 @@ source $locallib/actions.sh
 
 VERSION="0.7.1+svn"
 
-# All the path we'll need
-libdir="/usr/share/backup-manager"
-logger="/usr/bin/logger"
-zip="/usr/bin/zip"
-bzip="/usr/bin/bzip2"
-gpg="/usr/bin/gpg"
-lzma="/usr/bin/lzma"
-gzip="/bin/gzip"
-dar="/usr/bin/dar"
-_7z="/usr/bin/7z"
-tar="/bin/tar"
-rsync="/usr/bin/rsync"
-mkisofs="/usr/bin/mkisofs"
-growisofs="/usr/bin/growisofs"
-dvdrwformat="/usr/bin/dvd+rw-format"
-cdrecord="/usr/bin/cdrecord"
-bmu="/usr/bin/backup-manager-upload"
-lockfile="/var/run/backup-manager.pid"
-md5sum="/usr/bin/md5sum"
-bc="/usr/bin/bc"
-mysqldump="/usr/bin/mysqldump"
-svnadmin="/usr/bin/svnadmin"
+# external programs (cannot be sure where the are)
+zip=$(which zip)
+bzip=$(which bzip2)
+gzip=$(which gzip)
+gpg=$(which gpg)
+lzma=$(which lzma)
+dar=$(which dar)
+tar=$(which tar)
+rsync=$(which rsync)
+mkisofs=$(which mkisofs)
+growisofs=$(which growisofs)
+dvdrwformat=$(which dvd+rw-format)
+cdrecord=$(which cdrecord)
+md5sum=$(which md5sum)
+bc=$(which bc)
+mysqldump=$(which mysqldump)
+svnadmin=$(which svnadmin)
+logger=$(which logger)
 
+# Find which lockfile to use
+# If we are called by an unprivileged user, use a lockfile inside the user's home;
+# else, use /var/run/backup-manager.lock
+systemlockfile="/var/run/backup-manager.lock"
+userlockfile="$HOME/.backup-manager.lock"
+if [ "$UID" != 0 ]; then
+    lockfile="$userlockfile"
+else
+    lockfile="$systemlockfile"
+fi
 
 libdir="../lib"
 bmu="../backup-manager-upload"
-lockfile="$PWD/backup-manager.pid"
-lockfile="$PWD/test.lock"
 
 conffile="confs/base.conf"
 version="false"

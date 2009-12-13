8a0b10198384e94234fd43b756cef6efbf4c355f
Alexis Sukrieh <sukria@sukria.net>
2009-12-13 17:26:55 +0100

    test script for reproducing bug #235

---
+++ t/t20-hooks-bug235.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+# Each test script should include testlib.sh
+source testlib.sh
+# When the test is ready, set this to false for nice outputs.
+# if you want to see what happens, use those flags
+verbose="true"
+warnings="true"
+
+# The conffile part of the test, see confs/* for details.
+source confs/base.conf
+export BM_PRE_BACKUP_COMMAND="/NON_EXISTANT_FILE.pre.sh"
+export BM_POST_BACKUP_COMMAND="NON_EXISTANT_FILE.post.sh"
+source $locallib/sanitize.sh
+
+# The test actions
+bm_init_env
+bm_init_today
+exec_pre_command
+exec_post_command
+
+exit 0

b65692bb03035bef7eafc5cb2746d58ea16059cb
Dima Kogan <dima@secretsauce.net>
2015-11-20 21:14:38 -0800

    Replaced significant references to the defunct backup-manager.org

--- t/t11-pipe-method.sh
+++ t/t11-pipe-method.sh
@@ -8,7 +8,7 @@ source testlib.sh
 
 source confs/base.conf
 
-# taken verbatim from file attached to bug #4
+# taken verbatim from file attached to bug #4 that previously lived at
 # http://bugzilla.backup-manager.org/cgi-bin/attachment.cgi?id=1&action=view
 export BM_REPOSITORY_ROOT="$PWD/repository"
 export BM_ARCHIVE_METHOD="pipe"

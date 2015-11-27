0600c2d84e21416750782f100925f42dd23d981b
Dima Kogan <dima@secretsauce.net>
2015-11-26 20:29:35 -0800

    test suite makes sure to use the LOCAL perl files

--- t/testlib.sh
+++ t/testlib.sh
@@ -47,8 +47,8 @@ else
 fi
 
 libdir="../lib"
-bmu="../backup-manager-upload"
-bmp="../backup-manager-purge"
+bmu="perl -I.. ../backup-manager-upload"
+bmp="perl -I.. ../backup-manager-purge"
 
 conffile="confs/base.conf"
 version="false"

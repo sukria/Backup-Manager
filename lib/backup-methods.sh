7a43195d8feaa9d29f3bfb69e6312b0bf9c04ddc
Russ Odom <russ@gloomytrousers.co.uk>
2018-12-07 14:54:40 +0000

    Quote $pattern to prevent unwanted shell interpolation of wildcards

--- lib/backup-methods.sh
+++ lib/backup-methods.sh
@@ -253,7 +253,7 @@ function __get_flags_relative_blacklist()
     for pattern in $BM_TARBALL_BLACKLIST
     do
         # absolute paths
-        char=$(expr substr $pattern 1 1)
+        char=$(expr substr "$pattern" 1 1)
         if [[ "$char" = "/" ]]; then
 
            # we blacklist only absolute paths related to $target
@@ -261,13 +261,13 @@ function __get_flags_relative_blacklist()
 
                 # making a relative path...
                 pattern="${pattern#$target}"
-                length=$(expr length $pattern)
+                length=$(expr length "$pattern")
                 # for $target="/", no spare / is left at the beggining
                 # after the # substitution; thus take substr from pos 1
                 if [ "$target" != "/" ]; then
-                    pattern=$(expr substr $pattern 2 $length)
+                    pattern=$(expr substr "$pattern" 2 $length)
                 else
-                    pattern=$(expr substr $pattern 1 $length)
+                    pattern=$(expr substr "$pattern" 1 $length)
                 fi
 
                 # ...and blacklisting it
